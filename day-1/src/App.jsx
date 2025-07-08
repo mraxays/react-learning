82c9de8ccf635cb272dd2dea51b6ee64c755aa29
Mr Akshay Shinde <akshau72p@gmail.com>
2025-07-08 22:50:46 +0530

    Day 1 - Initialize React project with Vite, add basic App component, and include SVG assets

---
+++ day-1/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App(){
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a simple React component. {50 + 20}</p>
+    </div>
+  );
+}
+
+export default App;
