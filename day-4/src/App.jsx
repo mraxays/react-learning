2ca68489236fa38cbcd2c9b53cbad1c5933a982c
Mr Akshay Shinde <akshau72p@gmail.com>
2025-07-09 22:41:42 +0530

    Refactor App and Card components to display dynamic card data with images and descriptions

--- day-4/src/App.jsx
+++ day-4/src/App.jsx
@@ -1,26 +1,11 @@
 import React from "react";
-// import Card from './components/card'
+import Card from './components/card'
 
 function App() {
-  const data = [
-    "akshat",
-    "sachin",
-    "sagar",
-    "sahil",
-    "saurabh",
-    "shubham",
-    "siddharth",
-    "sourabh",
-    "sudhir",
-    "sumit",
-  ];
+
   return (
-    <div className="flex flex-wrap justify-center items-center gap-6">
-      {data.map((elem, index) => (
-        <div key={index} className="bg-gray-200 p-4 m-2 rounded-lg shadow-lg w-64">
-          <h1 className="text-xl font-bold capitalize">{elem}</h1>
-        </div>
-      ))}
+    <div className="">
+      <Card />
     </div>
   );
 }
