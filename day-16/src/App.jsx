d593e56d411ec2c926344d87d1ddcb2b6466769c
Mr Akshay Shinde <akshau72p@gmail.com>
2025-08-17 21:36:07 +0530

    Added left arrow icon and improved click functionality for image transition

--- day-16/src/App.jsx
+++ day-16/src/App.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
-import { FaRegArrowAltCircleRight } from "react-icons/fa";
+import { FaRegArrowAltCircleRight,FaRegArrowAltCircleLeft } from "react-icons/fa";
 
 function App() {
   const [val, setVal] = useState(false);
 
   return (
     <div className="p-4 flex justify-center items-center h-screen bg-gray-100">
-      <div className="w-sm flex rounded relative shadow-2xl border-3 border-gray-300 overflow-hidden">
+      <div className="w-sm flex rounded relative shadow-2xl border-3 border-gray-300 overflow-hidden select-none">
         <img
           className={`object-center object-cover shrink-0 w-sm aspect-video transition-transform duration-500 ${val ? "translate-x-0" : "-translate-x-full"}`}
           src="https://images.unsplash.com/photo-1717496002081-38a90d3cba26?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -19,7 +19,7 @@ function App() {
         />
         <div onClick={()=>setVal(()=>!val)} className="absolute inset-0 flex items-center justify-center text-black text-4xl transition hover:bg-amber-200/10 hover:text-amber-900">
           <span className="rounded-full bg-white p-1 shadow-lg">
-            <FaRegArrowAltCircleRight />
+            {val ? <FaRegArrowAltCircleLeft /> : <FaRegArrowAltCircleRight />}
           </span>
         </div>
       </div>
