94d6f0c52f853e32f072692dc1970f7fffe9bcd2
Mr Akshay Shinde <akshau72p@gmail.com>
2026-01-25 23:48:14 +0530

    feat: replace Link with NavLink for improved active link styling in navigation

--- day-23/src/components/Nav.jsx
+++ day-23/src/components/Nav.jsx
@@ -1,12 +1,47 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 function Nav() {
   return (
     <nav>
       <div className="flex gap-4 mb-4 bg-amber-200 p-4">
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-        <Link to="/contact">Contact</Link>
+          {/* <NavLink
+          className={(e) => {
+            return e.isActive ? "px-2 text-gray-200 bg-amber-400" : "px-2";
+          }}
+          to="/"
+        >
+          Home
+        </NavLink> */}
+        <NavLink
+          className={({ isActive }) =>
+            `px-2 transition-colors hover:bg-amber-500 ${
+              isActive ? "text-gray-200 bg-amber-400" : "text-gray-600"
+            }`
+          }
+          to="/"
+        >
+          Home
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `px-2 transition-colors hover:bg-amber-500 ${
+              isActive ? "text-gray-200 bg-amber-400" : "text-gray-600"
+            }`
+          }
+          to="/about"
+        >
+          About
+        </NavLink>
+        <NavLink
+          className={({ isActive }) =>
+            `px-2 transition-colors hover:bg-amber-500 ${
+              isActive ? "text-gray-200 bg-amber-400" : "text-gray-600"
+            }`
+          }
+          to="/contact"
+        >
+          Contact
+        </NavLink>
       </div>
     </nav>
   );
